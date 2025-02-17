c3b71a206d3d9922d6cf5b02e68dd16971081b3b
Lucianosc <luchotandil90@gmail.com>
2025-02-17 17:33:04 +0100

    feat: NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { motion } from "framer-motion";
+
+export default function NavBar() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="flex p-2 justify-center"
+    >
+      <motion.nav
+        layout
+        transition={{
+          duration: 0.5,
+          type: "spring",
+          bounce: 0.2,
+        }}
+        className="max-w-6xl flex justify-between items-center w-full bg-background2 rounded-2xl p-2"
+      >
+        <motion.h2 layout className="text-2xl font-bold">
+          Quantinium Assignment
+        </motion.h2>
+        <motion.div layout>
+          <ConnectButton />
+        </motion.div>
+      </motion.nav>
+    </motion.div>
+  );
+}
